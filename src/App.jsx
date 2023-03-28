5718dd5b93eabb483f265c8b86ead76f71e25aba
Sahrane_Guassemi <sahranedev@gmail.com>
2023-03-28 15:57:02 +0200

    new react quest prop 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,24 @@ import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+
+    {
+      name: "mew",
+    },
+  ];
+  const pokemon = pokemonList[0];
+  // changer l'index en pokemonList[1] pour afficher le deuxième élément du tableau et ainsi de suite
+
   return (
     <div className="App">
-      <PokemonCard />
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,6 @@
 import React from "react";
 
-const PokemonCard = () => {
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-
-    {
-      name: "mew",
-    },
-  ];
-  const pokemon = pokemonList[0];
-  // changer l'index en pokemonList[1] pour afficher le deuxième élément du tableau
+const PokemonCard = ({ pokemon }) => {
   return (
     <div>
       <figure>
