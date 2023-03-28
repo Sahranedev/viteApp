548188e8052801006a54e4754afa887eeda7d2fc
Sahrane_Guassemi <sahranedev@gmail.com>
2023-03-28 15:44:05 +0200

    new react quest 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 const PokemonCard = () => {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+
+    {
+      name: "mew",
+    },
+  ];
+  const pokemon = pokemonList[0];
+  // changer l'index en pokemonList[1] pour afficher le deuxième élément du tableau
   return (
     <div>
       <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-        <figcaption>bulbasaur</figcaption>
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : "???"}
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     </div>
   );
