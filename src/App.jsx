ef9211ec68e0dff435b86b1f9b2814faadbaacd9
Sahrane_Guassemi <sahranedev@gmail.com>
2023-03-29 10:36:12 +0200

    react quest state 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,50 @@
+import React, { useState } from "react";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
-import PropTypes from "prop-types";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
   const pokemonList = [
     {
       name: "bulbasaur",
-
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
-
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
     {
       name: "mew",
     },
   ];
-  const pokemon = pokemonList[0];
-  // changer l'index en pokemonList[1] pour afficher le deuxième élément du tableau et ainsi de suite
+
+  const handleNext = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+  const handlePrev = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
 
   return (
     <div className="App">
-      <PokemonCard pokemon={pokemon} />
+      <PokemonCard
+        pokemon={pokemonIndex}
+        handleNext={handleNext}
+        handlePrev={handlePrev}
+        pokemonList={pokemonList}
+      />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../pokemonCard.css";
 
-const PokemonCard = ({ pokemon }) => {
+const PokemonCard = ({ pokemon, handleNext, pokemonList, handlePrev }) => {
   PokemonCard.propTypes = {
     pokemon: PropTypes.shape({
       name: PropTypes.string.isRequired,
       imgSrc: PropTypes.string,
     }).isRequired,
   };
+
   return (
     <div>
       <figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
-        <figcaption>{pokemon.name}</figcaption>
+        {pokemonList[pokemon].imgSrc ? (
+          <img src={pokemonList[pokemon].imgSrc} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemonList[pokemon].name}</figcaption>
       </figure>
+      <div className="btn-container">
+        {pokemon < pokemonList.length - 1 && (
+          <button className="btn" onClick={handleNext}>
+            Suivant
+          </button>
+        )}
+
+        {pokemon > 0 && <button onClick={handlePrev}>Précédent</button>}
+      </div>
     </div>
   );
 };
