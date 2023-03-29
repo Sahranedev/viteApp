89cfcca1b65aad145ddba1c54ecadb9f90695d70
Sahrane_Guassemi <sahranedev@gmail.com>
2023-03-29 08:56:51 +0200

    proptypes quest

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const PokemonCard = ({ pokemon }) => {
+  PokemonCard.propTypes = {
+    pokemon: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
+    }).isRequired,
+  };
   return (
     <div>
       <figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : "???"}
+        {pokemon.imgSrc ? <img src={pokemon.imgSrc} /> : <p>???</p>}
         <figcaption>{pokemon.name}</figcaption>
       </figure>
     </div>
